9124396d12aa2836437b2184ed9075f83adfe24b
silberlima <silber.lima@gmail.com>
2021-09-30 14:59:45 -0300

    Resilience4j

---
+++ book-service/src/main/java/br/com/silber/controller/FooBarController.java
@@ -0,0 +1,33 @@
+package br.com.silber.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+
+@RestController
+@RequestMapping("book-service")
+public class FooBarController {
+
+	private Logger logger = LoggerFactory.getLogger(FooBarController.class);
+	
+	@GetMapping("/foo-bar")
+//	@Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
+//	@CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
+	@RateLimiter(name = "default")
+	public String fooBar() {
+		logger.info("Request to foo-bar is received");
+		//var response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
+		//return response.getBody();
+		return "Foo-Bar";
+	};
+	
+	public String fallbackMethod(Exception e) {
+		return "fallbackMethod foo-bar! ";
+	}
+}
